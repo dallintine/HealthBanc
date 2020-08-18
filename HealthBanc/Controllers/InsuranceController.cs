062c2b574cda77dc6517e80426b341973381136c
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-08-18 13:42:44 +0100

    Update InsuranceController.cs

--- HealthBanc/Controllers/InsuranceController.cs
+++ HealthBanc/Controllers/InsuranceController.cs
@@ -40,13 +40,13 @@ namespace HealthBanc.Controllers
             {
                 //var result = _insuranceService.AxaMansardGetToken(userName, password);
                 //StringReader stringReader = new StringReader(result);
-                ////var stringReader = @"<?xml version=""1.0"" encoding=""utf - 8""?><soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema""><soap:Body><getTokenResponse xmlns=""http://tempuri.org/""><getTokenResult><IsSuccessful>true</IsSuccessful><message>V2NXM01D2EgGUGzHgbbBTbxPAeFY/LokUwBUAEIAQQBOAEsAMAAxAA==</message><ReturnedObject xsi:type=""xsd:string"">STERLINGBANK</ReturnedObject><ReturnCode>00</ReturnCode></getTokenResult></getTokenResponse></soap:Body></soap:Envelope>";
-                ////StringReader stringReaders = new StringReader(stringReader);
+                //var stringReader = @"<?xml version=""1.0"" encoding=""utf - 8""?><soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema""><soap:Body><getTokenResponse xmlns=""http://tempuri.org/""><getTokenResult><IsSuccessful>true</IsSuccessful><message>V2NXM01D2EgGUGzHgbbBTbxPAeFY/LokUwBUAEIAQQBOAEsAMAAxAA==</message><ReturnedObject xsi:type=""xsd:string"">STERLINGBANK</ReturnedObject><ReturnCode>00</ReturnCode></getTokenResult></getTokenResponse></soap:Body></soap:Envelope>";
+                //StringReader stringReaders = new StringReader(stringReader);
                 ////XmlDocument doc = new XmlDocument();
                 ////doc.LoadXml(stringReader);
-                ////string json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+                //string json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
                 //XmlSerializer serializer = new XmlSerializer(typeof(Envelope));
-                //Envelope getToken = (Envelope)serializer.Deserialize(stringReader);
+                //Envelope getToken = (Envelope)serializer.Deserialize(stringReaders);
 
                 var result = _insuranceService.AxaMansardGetToken(userName, password);
                 if(result.Status == true)
