8278bb2c3b6988710b1ae69ba220a9e1ae2fd41a
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-08-18 12:26:57 +0100

    Update GetTokenResult.cs

--- HealthBanc/Response/AxaMansard/GetTokenResult.cs
+++ HealthBanc/Response/AxaMansard/GetTokenResult.cs
@@ -14,9 +14,18 @@ namespace HealthBanc.Response.AxaMansard
 		[XmlElement(ElementName = "message", Namespace = "http://tempuri.org/")]
 		public string Message { get; set; }
 		[XmlElement(ElementName = "ReturnedObject", Namespace = "http://tempuri.org/")]
-		public string ReturnedObject { get; set; }
+		public ReturnedObject ReturnedObject { get; set; }
 		[XmlElement(ElementName = "ReturnCode", Namespace = "http://tempuri.org/")]
 		public string ReturnCode { get; set; }
+	}
+
+	[XmlRoot(ElementName = "ReturnedObject", Namespace = "http://tempuri.org/")]
+	public class ReturnedObject
+	{
+		[XmlAttribute(AttributeName = "type", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+		public string Type { get; set; }
+		[XmlText]
+		public string Text { get; set; }
 	}
 
 	[XmlRoot(ElementName = "getTokenResponse", Namespace = "http://tempuri.org/")]
@@ -24,6 +33,8 @@ namespace HealthBanc.Response.AxaMansard
 	{
 		[XmlElement(ElementName = "getTokenResult", Namespace = "http://tempuri.org/")]
 		public GetTokenResult GetTokenResult { get; set; }
+		[XmlAttribute(AttributeName = "xmlns")]
+		public string Xmlns { get; set; }
 	}
 
 	[XmlRoot(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
@@ -34,15 +45,15 @@ namespace HealthBanc.Response.AxaMansard
 	}
 
 	[XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
-	public class EnvelopeHome
+	public class Envelope
 	{
 		[XmlElement(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
 		public Body Body { get; set; }
+		[XmlAttribute(AttributeName = "soap", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Soap { get; set; }
 		[XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2000/xmlns/")]
 		public string Xsi { get; set; }
 		[XmlAttribute(AttributeName = "xsd", Namespace = "http://www.w3.org/2000/xmlns/")]
 		public string Xsd { get; set; }
-		[XmlAttribute(AttributeName = "soap", Namespace = "http://www.w3.org/2000/xmlns/")]
-		public string Soap { get; set; }
 	}
 }
