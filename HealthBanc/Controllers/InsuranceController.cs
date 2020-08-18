7594da49af9e38c262556faca2374ae248c2993a
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-08-18 13:02:02 +0100

    Update InsuranceController.cs

--- HealthBanc/Controllers/InsuranceController.cs
+++ HealthBanc/Controllers/InsuranceController.cs
@@ -38,20 +38,30 @@ namespace HealthBanc.Controllers
         {           
             try
             {
+                //var result = _insuranceService.AxaMansardGetToken(userName, password);
+                //StringReader stringReader = new StringReader(result);
+                ////var stringReader = @"<?xml version=""1.0"" encoding=""utf - 8""?><soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema""><soap:Body><getTokenResponse xmlns=""http://tempuri.org/""><getTokenResult><IsSuccessful>true</IsSuccessful><message>V2NXM01D2EgGUGzHgbbBTbxPAeFY/LokUwBUAEIAQQBOAEsAMAAxAA==</message><ReturnedObject xsi:type=""xsd:string"">STERLINGBANK</ReturnedObject><ReturnCode>00</ReturnCode></getTokenResult></getTokenResponse></soap:Body></soap:Envelope>";
+                ////StringReader stringReaders = new StringReader(stringReader);
+                ////XmlDocument doc = new XmlDocument();
+                ////doc.LoadXml(stringReader);
+                ////string json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
+                //XmlSerializer serializer = new XmlSerializer(typeof(Envelope));
+                //Envelope getToken = (Envelope)serializer.Deserialize(stringReader);
+
                 var result = _insuranceService.AxaMansardGetToken(userName, password);
-                StringReader stringReader = new StringReader(result);
-                //var stringReader = @"<?xml version=""1.0"" encoding=""utf - 8""?><soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema""><soap:Body><getTokenResponse xmlns=""http://tempuri.org/""><getTokenResult><IsSuccessful>true</IsSuccessful><message>V2NXM01D2EgGUGzHgbbBTbxPAeFY/LokUwBUAEIAQQBOAEsAMAAxAA==</message><ReturnedObject xsi:type=""xsd:string"">STERLINGBANK</ReturnedObject><ReturnCode>00</ReturnCode></getTokenResult></getTokenResponse></soap:Body></soap:Envelope>";
-                //StringReader stringReaders = new StringReader(stringReader);
-                //XmlDocument doc = new XmlDocument();
-                //doc.LoadXml(stringReader);
-                //string json = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc);
-                XmlSerializer serializer = new XmlSerializer(typeof(Envelope));
-                Envelope getToken = (Envelope)serializer.Deserialize(stringReader);
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(result);
+                var getToken = new GetTokenResult();
+                getToken.ReturnCode = xmlDoc.GetElementsByTagName("ReturnCode").Item(0).InnerText;
+                getToken.IsSuccessful = xmlDoc.GetElementsByTagName("IsSuccessful").Item(0).InnerText;
+                getToken.Message = xmlDoc.GetElementsByTagName("Message").Item(0).InnerText;
+
                 return Ok(getToken);
+
             }
             catch(Exception ex)
             {
-                return BadRequest(ex );
+                return BadRequest(ex);
             }
         }
 
