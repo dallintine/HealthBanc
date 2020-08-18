4af8b4225c06d42c1743a5d35d29c6e3942a790a
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-08-18 22:39:22 +0100

    Update  Jwt settings
    Update  Jwt settings

--- HealthBanc/Domain/Models/RefreshToken.cs
+++ HealthBanc/Domain/Models/RefreshToken.cs
@@ -8,14 +8,13 @@ namespace HealthBanc.Domain.Models
 {
     public class RefreshToken
     {
-        public string Token { get; set; }
-        public string JwtId { get; set; }
-        public DateTime CreationDate { get; set; }
-        public DateTime ExpiryDate { get; set; }
-        public bool Used { get; set; }
-        public bool Invalidated { get; set; }
-
-        public int UserId { get; set; }
-        public IdentityUser user { get; set; }
+        //public string Token { get; set; }
+        //public string JwtId { get; set; }
+        //public DateTime CreationDate { get; set; }
+        //public DateTime ExpiryDate { get; set; }
+        //public bool Used { get; set; }
+        //public bool Invalidated { get; set; }
+        //public int UserId { get; set; }
+        //public IdentityUser user { get; set; }
     }
 }
