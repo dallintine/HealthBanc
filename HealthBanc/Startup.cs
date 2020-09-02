cb9b676c53c1ca95dad7ad9e2aa153c91efb471d
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-09-02 08:28:25 +0100

    Update Startup.cs

--- HealthBanc/Startup.cs
+++ HealthBanc/Startup.cs
@@ -311,7 +311,10 @@ namespace HealthBanc
 
             app.Use(async (context, next) =>
             {
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
                 context.Response.Headers.Add("X-Frame-Options", "DENY");
+                context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+                context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
                 //context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
                 context.Response.Headers.Add("Feature-Policy", "accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none';");
                 await next();
