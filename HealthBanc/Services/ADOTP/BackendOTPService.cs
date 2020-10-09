1030c18221517d2d5d057d13c167fbfc5d20f42a
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-10-09 18:11:28 +0100

    Update BackendOTPService.cs

--- HealthBanc/Services/ADOTP/BackendOTPService.cs
+++ HealthBanc/Services/ADOTP/BackendOTPService.cs
@@ -20,7 +20,7 @@ namespace HealthBanc.Services.ADOTP
     {
         public readonly HealthBanc.Helpers.SterlingOtp Options;
         private readonly ILogger<BackendOTPService> _logger;
-        public readonly string serviceUrl = "https://az-cpibap2-serv/OTPCentralService.asmx";
+        public readonly string serviceUrl = "https://az-cpibap2-serv/OTPCentralService.asmx?op=OtpValidation";
         public readonly EndpointAddress endpointAddress;
         public readonly BasicHttpBinding basicHttpBinding;
 
