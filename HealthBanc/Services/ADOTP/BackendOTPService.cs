40cb4245eaef76eb1965f2a9c30e8725c458b12d
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-10-09 12:18:39 +0100

    Update BackendOTPService.cs

--- HealthBanc/Services/ADOTP/BackendOTPService.cs
+++ HealthBanc/Services/ADOTP/BackendOTPService.cs
@@ -39,6 +39,7 @@ namespace HealthBanc.Services.ADOTP
             basicHttpBinding.CloseTimeout = TimeSpan.MaxValue;
             basicHttpBinding.ReceiveTimeout = TimeSpan.MaxValue;
             basicHttpBinding.SendTimeout = TimeSpan.MaxValue;
+            basicHttpBinding.UseDefaultWebProxy = true;
         }
 
         public string SOAPManual(string otp, string username)
