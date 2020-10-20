b87af2af013f5b7f09540c755b916619e714b168
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-10-20 11:26:22 +0100

    Update DomainToDTOProfile.cs

--- HealthBanc/Mappers/DomainToDTOProfile.cs
+++ HealthBanc/Mappers/DomainToDTOProfile.cs
@@ -16,11 +16,9 @@ namespace HealthBanc.Mappers
 {
     public class DomainToDTOProfile : Profile
     {
-        private Paystack Options { get; }
 
-        public DomainToDTOProfile(IOptions<Paystack> optionAccessor)
+        public DomainToDTOProfile()
         {
-            Options = optionAccessor.Value;
 
             CreateMap<Service, ServiceBreakdown>();
             CreateMap<ApplicationUser, ApplicationUserDTO>();
@@ -30,16 +28,16 @@ namespace HealthBanc.Mappers
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(x => x.Surname))
                 .ForMember(dest => dest.EmailAddress, opt => opt.MapFrom(x => x.Email))
                 .ForMember(dest => dest.RepaymentAmount, opt => opt.MapFrom(x => Convert.ToInt32(x.Premium)))
-                .ForMember(dest => dest.Channel, opt => opt.MapFrom(x => Options.Config.Channel))
-                .ForMember(dest => dest.TokenType, opt => opt.MapFrom(x => Options.Config.TokenType));
+                .ForMember(dest => dest.Channel, opt => opt.MapFrom(x => "healthinsured"))
+                .ForMember(dest => dest.TokenType, opt => opt.MapFrom(x => "paystack"));
 
             CreateMap<AxaMansardUserProfile, SubscribePayment>()
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(x => x.Othernames))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(x => x.Surname))
                 .ForMember(dest => dest.EmailAddress, opt => opt.MapFrom(x => x.Email))
                 .ForMember(dest => dest.RepaymentAmount, opt => opt.MapFrom(x => Convert.ToInt32(x.Premium)))
-                .ForMember(dest => dest.Channel, opt => opt.MapFrom(x => Options.Config.Channel))
-                .ForMember(dest => dest.TokenType, opt => opt.MapFrom(x => Options.Config.TokenType))
+                .ForMember(dest => dest.Channel, opt => opt.MapFrom(x => "healthinsured"))
+                .ForMember(dest => dest.TokenType, opt => opt.MapFrom(x => "paystack"))
                 .ForMember(dest => dest.NextRepaymentDate, opt => opt.MapFrom(x => DateTime.Now.AddMonths(1)));
 
 
