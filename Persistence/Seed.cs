d6c86439af83f8649e31a79ae3a2618cb593a2ab
hassan hassan <hassan.hassan@sterling.ng>
2021-01-12 14:29:55 +0000

    Updated Seed.cs

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -12,7 +12,7 @@ namespace Persistence
     {
         public static async Task SeedData(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            if(userManager.FindByEmailAsync("hassan.hassan@sterling.ng") != null)
+            if(userManager.FindByEmailAsync("hassan.hassan@sterling.ng") == null)
             {
                 ApplicationUser user = new ApplicationUser()
                 {
@@ -28,6 +28,62 @@ namespace Persistence
                     Email = "hassan.hassan@sterling.ng",
                     FirstName = "Hassan",
                     LastName = "Hassan",
+                    ClassOrRoleId = 6
+                };
+
+                var result = userManager.CreateAsync(user).Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Super-Administrator").Wait();
+                    context.BackendAdminUsers.Add(adminUser);
+                    await context.SaveChangesAsync();
+                }
+            }
+            if (userManager.FindByEmailAsync("Esther.nwowo@sterling.ng") == null)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UniqueUsername = "nwowore",
+                    UserName = "Esther.nwowo@sterling.ng",
+                    Email = "Esther.nwowo@sterling.ng",
+                    FirstName = "Esther",
+                    LastName = "Nwowo",
+                    EmailConfirmed = true
+                };
+                BackendAdminUser adminUser = new BackendAdminUser()
+                {
+                    Email = "Esther.nwowo@sterling.ng",
+                    FirstName = "Esther",
+                    LastName = "Nwowo",
+                    ClassOrRoleId = 6
+                };
+
+                var result = userManager.CreateAsync(user).Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Super-Administrator").Wait();
+                    context.BackendAdminUsers.Add(adminUser);
+                    await context.SaveChangesAsync();
+                }
+            }
+            if (userManager.FindByEmailAsync("constance.okosodo@sterling.ng") == null)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UniqueUsername = "Okosodoec",
+                    UserName = "constance.okosodo@sterling.ng",
+                    Email = "constance.okosodo@sterling.ng",
+                    FirstName = "Constance",
+                    LastName = "Okosodo",
+                    EmailConfirmed = true
+                };
+                BackendAdminUser adminUser = new BackendAdminUser()
+                {
+                    Email = "constance.okosodo@sterling.ng",
+                    FirstName = "Constance",
+                    LastName = "Okosodo",
                     ClassOrRoleId = 6
                 };
 
