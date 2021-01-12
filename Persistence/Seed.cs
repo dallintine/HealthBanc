7ddf679ab38205722037e1e11ddb4c2728278643
hassan hassan <hassan.hassan@sterling.ng>
2021-01-12 14:54:26 +0000

    Updated Seed.cs

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -12,7 +12,7 @@ namespace Persistence
     {
         public static async Task SeedData(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            if(userManager.FindByEmailAsync("hassan.hassan@sterling.ng") == null)
+            if(userManager.FindByEmailAsync("hassan.hassan@sterling.ng") != null)
             {
                 ApplicationUser user = new ApplicationUser()
                 {
@@ -40,7 +40,7 @@ namespace Persistence
                     await context.SaveChangesAsync();
                 }
             }
-            if (userManager.FindByEmailAsync("Esther.nwowo@sterling.ng") == null)
+            if (userManager.FindByEmailAsync("Esther.nwowo@sterling.ng") != null)
             {
                 ApplicationUser user = new ApplicationUser()
                 {
@@ -68,7 +68,7 @@ namespace Persistence
                     await context.SaveChangesAsync();
                 }
             }
-            if (userManager.FindByEmailAsync("constance.okosodo@sterling.ng") == null)
+            if (userManager.FindByEmailAsync("constance.okosodo@sterling.ng") != null)
             {
                 ApplicationUser user = new ApplicationUser()
                 {
