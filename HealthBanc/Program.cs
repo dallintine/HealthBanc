d1da98c94764de842902f1436611ae336ec61177
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2021-01-27 20:53:03 +0100

    Update Program.cs

--- HealthBanc/Program.cs
+++ HealthBanc/Program.cs
@@ -28,8 +28,7 @@ namespace HealthBanc
         //    {
         //        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
         //        optionsBuilder.UseSqlServer("Server=10.0.41.101; Database=HealthBanc; User ID=sa; Password=tylent; Trusted_Connection=False; MultipleActiveResultSets=true");
-        //        //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HealthBancNew;Trusted_Connection=True;MultipleActiveResultSets=true");
-
+        //        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HealthBanc;Trusted_Connection=True;MultipleActiveResultSets=true");
         //        return new ApplicationDbContext(optionsBuilder.Options);
         //    }
         //}
