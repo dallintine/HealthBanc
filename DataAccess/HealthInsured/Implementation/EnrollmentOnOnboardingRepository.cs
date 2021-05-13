d70c8b7892d53ef282cf4523d87572d94fee9d51
Hassan Hassan <hassan.olatade.hh@gmail.com>
2021-05-13 19:40:59 +0100

    Update EnrollmentOnOnboardingRepository.cs

--- DataAccess/HealthInsured/Implementation/EnrollmentOnOnboardingRepository.cs
+++ DataAccess/HealthInsured/Implementation/EnrollmentOnOnboardingRepository.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,7 +19,7 @@ namespace DataAccess.HealthInsured.Implementation
 
         public async Task<EnrollmentOnOnboarding> GetLastEnrollmentByInsuranceProfileId(int insuranceProfileId)
         {
-            return await _context.EnrollmentOnOnboardings.LastOrDefaultAsync(x => x.InsuranceUserProfileId == insuranceProfileId);
+            return await _context.EnrollmentOnOnboardings.Where(x => x.InsuranceUserProfileId == insuranceProfileId).LastOrDefaultAsync();
         }
     }
 }
