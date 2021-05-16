c5b8495f1a2688335adf2125c52b8d318ee6fcde
Hassan Hassan <hassan.olatade.hh@gmail.com>
2021-05-16 09:59:14 +0100

    update logs extension class
    update logs extension class

--- HealthBanc/Logging/Extensions.cs
+++ HealthBanc/Logging/Extensions.cs
@@ -26,7 +26,7 @@ namespace HealthBanc.Logging
                     .MinimumLevel.Is(level)
                     .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
                     .Enrich.WithProperty("ApplicationName", applicationName);
-                Configure(loggerConfiguration, Serilog.Events.LogEventLevel.Information, seqOptions, serilogOptions,azureBlobOptions, applicationInsightsOptions);
+                Configure(loggerConfiguration, Serilog.Events.LogEventLevel.Warning, seqOptions, serilogOptions,azureBlobOptions, applicationInsightsOptions);
             });
 
         private static void Configure(LoggerConfiguration loggerConfiguration, LogEventLevel level,SeqOptions seqOptions, SerilogOptions serilogOptions,AzureBlobOptions azureBlobOptions,
