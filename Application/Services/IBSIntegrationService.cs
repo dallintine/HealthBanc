32bb8d84c027eed01ea40e0c512c935a79ab0063
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2021-06-24 16:59:53 +0100

    update IBSIntegration service
    update IBSIntegration service

--- Application/Services/IBSIntegrationService.cs
+++ Application/Services/IBSIntegrationService.cs
@@ -16,7 +16,7 @@ namespace Application.Services
 {
     public class IBSIntegrationService
     {
-        public readonly string serviceUrl = "http://10.0.41.102:818/IBSServices.asmx";
+        public readonly string serviceUrl = "http://10.0.41.189:833/IBSServices.asmx";
         public readonly EndpointAddress endpointAddress;
         public readonly BasicHttpBinding basicHttpBinding;
         private readonly ILogger<IBSIntegrationService> _logger;
