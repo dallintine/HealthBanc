ce420ed5452fbd446143334f417f775f2c8442d4
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2021-06-29 09:10:55 +0100

    Modified connection string in appsettings.json
    Modified connection string in appsettings.json

--- HealthBanc/Program.cs
+++ HealthBanc/Program.cs
@@ -27,8 +27,8 @@ namespace HealthBanc
             public ApplicationDbContext CreateDbContext(string[] args)
             {
                 var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-                //optionsBuilder.UseSqlServer("Server=10.0.41.101; Database=HealthBancHygeiaImplementation; User ID=sa; Password=tylent; Trusted_Connection=False; MultipleActiveResultSets=true");
-                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HealthBancV3.2;Trusted_Connection=True;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer("Server=10.0.41.101; Database=HealthBancHygeiaImplementation; User ID=sa; Password=tylent; Trusted_Connection=False; MultipleActiveResultSets=true");
+                //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HealthBancV3.2;Trusted_Connection=True;MultipleActiveResultSets=true");
 
                 return new ApplicationDbContext(optionsBuilder.Options);
             }
