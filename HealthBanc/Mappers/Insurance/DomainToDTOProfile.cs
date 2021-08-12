d624d4825387aad98a781688c3fd3a90d73f3f41
Hassan <hassan.olatade.hh@gmail.com>
2021-08-12 22:58:53 +0100

    Update DomainToDTOProfile.cs

--- HealthBanc/Mappers/Insurance/DomainToDTOProfile.cs
+++ HealthBanc/Mappers/Insurance/DomainToDTOProfile.cs
@@ -36,6 +36,8 @@ namespace HealthBanc.Mappers.Insurance
             CreateMap<InsuranceUserProfile, TransactionLogDTO>()
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(x => x.Surname + " " + x.Othernames));
 
+            CreateMap<InsuranceUserProfile, FamilyMembersDTO>();
+
             //CreateMap<PaymentReference, TransactionLogDTO>().IncludeMembers(x => x.InsuranceUserProfile).IncludeMembers(x => x.CompanyProfile);
         }
     }
