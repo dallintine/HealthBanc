1d6dfdcc7f9682b656084f311eab9aed57510ae7
Holatade <hassan.olatade.hh@gmail.com>
2021-11-03 23:53:37 +0100

    Update MyAuthorizationFilter.cs

--- HealthBanc/MyAuthorizationFilter.cs
+++ HealthBanc/MyAuthorizationFilter.cs
@@ -29,8 +29,6 @@ namespace HealthBanc
 
             var access_token = String.Empty;
             var setCookie = false;
-            
-            return true;
 
             // try to get token from query string
             if (httpContext.Request.Query.ContainsKey("access_token"))
