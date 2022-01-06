91e001929e6e0c7b9a2396ba7e370289d168dbb4
Holatade <hassan.olatade.hh@gmail.com>
2022-01-06 16:09:40 +0100

    Update Extensions.cs

--- HealthBanc/Logging/Extensions.cs
+++ HealthBanc/Logging/Extensions.cs
@@ -47,7 +47,7 @@ namespace HealthBanc.Logging
             if (azureBlobOptions.Enabled)
             {
                 loggerConfiguration.WriteTo.AzureBlobStorage(azureBlobOptions.ConnectionString, level, azureBlobOptions.StorageContainerName, azureBlobOptions.StorageFileName
-                   , azureBlobOptions.OutputTemplate, false, null, null, false, null, null, 3000000, null);
+                   , azureBlobOptions.OutputTemplate, false, null, null, false, null, null, 1100000, null);
                 loggerConfiguration.MinimumLevel.Verbose().MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", Serilog.Events.LogEventLevel.Warning);
                 loggerConfiguration.MinimumLevel.Verbose().MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Query", Serilog.Events.LogEventLevel.Warning);
             }
