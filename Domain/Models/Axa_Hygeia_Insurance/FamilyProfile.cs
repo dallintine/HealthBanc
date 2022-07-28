0d10ca7fa68c234c26fc47068bdd282d4d8d9e15
Hassan Hassan <hassan.olatade.hh@gmail.com>
2022-07-28 11:11:41 +0100

    Update companyProfile
    Update companyProfile

--- Domain/Models/Axa_Hygeia_Insurance/FamilyProfile.cs
+++ Domain/Models/Axa_Hygeia_Insurance/FamilyProfile.cs
@@ -53,7 +53,7 @@ namespace Domain.Models.Axa_Hygeia_Insurance
         public DateTime DateCreated { get; set; }
         public List<InsuranceUserProfile> InsuranceUserProfiles { get; set; }
         public List<DebitCard> Cards { get; set; }
-        public List<PaymentReference> PaymentReferences { get; set; }
+        //public List<PaymentReference> PaymentReferences { get; set; }
         public List<ActivityLog> HealthInsuredActivityLogs { get; set; }
     }
 }
