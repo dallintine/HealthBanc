20ec6ebce60092bc1138c52802b04c41360e7a2f
Hassan Hassan <hassan.olatade.hh@gmail.com>
2023-07-15 19:34:18 +0100

    Update RegisterCommand.cs

--- Application/Identity/Commands/RegisterCommand.cs
+++ Application/Identity/Commands/RegisterCommand.cs
@@ -14,6 +14,7 @@ namespace Application.Identity.Commands
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
+        public string Address { get; set; }
         public string PhoneNumber { get; set; }
     }
 }
